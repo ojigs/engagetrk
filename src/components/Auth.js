060bb9057a01f3823dc1c7d2adc954dfb36fbaa2
Emmanuel Ojighoro <awesomeemoj@gmail.com>
2023-05-03 14:29:21 +0100

    styling: stripped redundant classes

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -53,45 +53,32 @@ const Auth = () => {
   };
 
   return (
-    <div
-      className={
-        "w-full h-full sm:h-auto sm:w-2/5 max-w-sm p-5 bg-white shadow flex col text-base"
-      }
-    >
-      <span
-        className={
-          "font-sans text-4xl text-center pb-2 mb-1 border-b mx-4 align-center"
-        }
-      >
+    <div className={"p-5 bg-white shadow flex col"}>
+      <span className={"text-center pb-2 mb-1 border-b mx-4 align-center"}>
         Login
       </span>
-      <label className={"mt-3 mb-2 font-medium text-lg"} htmlFor={"email"}>
-        <span className={"font-mono mr-1 text-red-400"}>*</span>Email:
+      <label className={"mt-3 mb-2 text-lg"} htmlFor={"email"}>
+        <span className={"mr-1"}>*</span>Email:
       </label>
       <input
-        className={"bg-gray-100 border py-1 px-3"}
+        className={"border py-1 px-3"}
         type={"email"}
         name={"email"}
         ref={emailRef}
         required
       />
-      <label className={"mt-3 mb-2 font-medium text-lg"} htmlFor={"password"}>
-        <span className={"font-mono mr-1 text-red-400"}>*</span>
+      <label className={"mt-3 mb-2 text-lg"} htmlFor={"password"}>
+        <span className={"mr-1 text-red-400"}>*</span>
         Password:
       </label>
       <input
-        className={"bg-gray-100 border py-1 px-3"}
+        className={"border py-1 px-3"}
         type={"password"}
         name={"password"}
         ref={passwordRef}
         required
       />
-      <span
-        className={
-          "text-blue-600 mt-2 cursor-pointer self-end text-sm font-medium"
-        }
-        onClick={forgotPassword}
-      >
+      <span className={"mt-2 text-sm"} onClick={forgotPassword}>
         Forgot Password?
       </span>
       {!!helperText.text && (
@@ -106,22 +93,20 @@ const Auth = () => {
         </div>
       )}
       <div className="mt-2 flex">
-        <span className="block mx-1.5 w-full rounded-md shadow-sm">
+        <span className="mx-1.5 shadow-sm">
           <button
             type="submit"
             onClick={() => handleLogin("REGISTER").catch(console.error)}
-            className={
-              "border w-full border-blue-600 text-blue-600 flex justify-center py-2 px-4 text-sm font-medium rounded-md hover:bg-blue-200 focus:outline-none focus:border-blue-700 focus:shadow-outline-blue active:bg-blue-700 transition duration-150 ease-in-out"
-            }
+            className={"w-full flex justify-center py-2 px-4"}
           >
             Sign Up
           </button>
         </span>
-        <span className="block w-full mx-1.5 rounded-md shadow-sm">
+        <span className="">
           <button
             onClick={() => handleLogin("LOGIN")}
             type="button"
-            className="flex w-full justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-500 focus:outline-none focus:border-blue-700 focus:shadow-outline-blue active:bg-blue-700 transition duration-150 ease-in-out"
+            className="flex w-full justify-center py-2 px-4"
           >
             Sign In
           </button>
@@ -129,23 +114,21 @@ const Auth = () => {
       </div>
       <div className="mt-3">
         <div className="relative">
-          <div className="absolute inset-0 flex items-center">
-            <div className="w-full mx-1.5 border-t border-gray-300" />
+          <div className="flex items-center">
+            <div className="" />
           </div>
-          <div className="relative flex justify-center text-sm leading-5">
-            <span className="px-2 bg-white text-gray-500">
-              Or continue with
-            </span>
+          <div className="flex justify-center">
+            <span className="px-2 bg-white ">Or continue with</span>
           </div>
         </div>
 
         <div>
           <div className="mt-3">
-            <span className="block rounded-md shadow-sm">
+            <span className="">
               <button
                 onClick={() => handleOAuthLogin("github")}
                 type="button"
-                className="w-3/4 mx-auto flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-500 focus:outline-none focus:border-blue-700 focus:shadow-outline-blue active:bg-blue-700 transition duration-150 ease-in-out"
+                className="mx-auto flex justify-center py-2 px-4"
               >
                 GitHub
               </button>
@@ -156,7 +139,7 @@ const Auth = () => {
               <button
                 onClick={() => handleOAuthLogin("google")}
                 type="button"
-                className="w-3/4 mx-auto flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-500 focus:outline-none focus:border-blue-700 focus:shadow-outline-blue active:bg-blue-700 transition duration-150 ease-in-out"
+                className="w-3/4 mx-auto flex justify-center py-2 px-4"
               >
                 Google
               </button>
