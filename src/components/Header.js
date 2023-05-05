95e8ce3c968c6278e8bc4cdb5e5d01baef5fcec5
Emmanuel Ojighoro <awesomeemoj@gmail.com>
2023-05-05 17:23:25 +0100

    styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -22,31 +22,33 @@ const Header = ({ session }) => {
 
   return (
     <header>
-      <Navbar className={`${navBackground} py-3`} expand="lg">
+      <Navbar className={`${navBackground} py-2`} expand="lg">
         <Container>
           <Navbar.Brand>
             <img src="./brnd.png" alt="" /> Engagetrk
           </Navbar.Brand>
-          <Navbar.Toggle
-            aria-controls="basic-navbar-nav"
-            onClick={toogleNavBg}
-          />
           {session && (
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="ms-auto">
-                <Nav.Item className="">
-                  <Nav.Link href="">
-                    <FontAwesomeIcon color="blue" icon={solid("bell")} />
-                  </Nav.Link>
-                </Nav.Item>
-                <Nav.Item className="">
-                  <Nav.Link href="/">Home</Nav.Link>
-                </Nav.Item>
-                <Nav.Item className="">
-                  <Nav.Link onClick={handleLogout}>Logout</Nav.Link>
-                </Nav.Item>
-              </Nav>
-            </Navbar.Collapse>
+            <>
+              <Navbar.Toggle
+                aria-controls="basic-navbar-nav"
+                onClick={toogleNavBg}
+              />
+              <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="ms-auto">
+                  <Nav.Item className="">
+                    <Nav.Link href="">
+                      <FontAwesomeIcon color="blue" icon={solid("bell")} />
+                    </Nav.Link>
+                  </Nav.Item>
+                  <Nav.Item className="">
+                    <Nav.Link href="/">Home</Nav.Link>
+                  </Nav.Item>
+                  <Nav.Item className="">
+                    <Nav.Link onClick={handleLogout}>Logout</Nav.Link>
+                  </Nav.Item>
+                </Nav>
+              </Navbar.Collapse>
+            </>
           )}
         </Container>
       </Navbar>
