c1104fa47235905c7b0839e0378eba8c2edd47c1
Emmanuel Ojighoro <awesomeemoj@gmail.com>
2023-08-06 01:18:14 +0100

    UI improvement and code cleanup

--- src/components/TodoListItem/TodoListItem.js
+++ src/components/TodoListItem/TodoListItem.js
@@ -1,41 +1,17 @@
-// import { useDispatch } from "react-redux";
-import { Button, Card, Spinner, Container } from "react-bootstrap";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { solid } from "@fortawesome/fontawesome-svg-core/import.macro";
+import { Spinner, Container } from "react-bootstrap";
 import UpdateTodo from "../UpdateTodo";
 import TodoCard from "../TodoCard";
-import {
-  useDeleteTodoMutation,
-  useGetTodosQuery,
-} from "../services/features/apiSlice";
+import { useGetTodosQuery } from "../services/features/apiSlice";
 import { useCallback, useEffect, useState } from "react";
 
 const TodoListItem = ({ handleMessage }) => {
   const [showUpdateModal, setShowUpdateModal] = useState(false);
   const [selectedTodo, setSelectedTodo] = useState();
-  const [deletingId, setDeletingID] = useState(null);
   const { data: todoItems, isLoading, isError, error } = useGetTodosQuery();
-
-  const [deleteTodo] = useDeleteTodoMutation();
 
   useEffect(() => {
     if (isError) console.error(error);
   }, [isError, error]);
-
-  const handleDeleteTodo = useCallback(
-    async (e, id) => {
-      try {
-        setDeletingID(id);
-        await deleteTodo(id);
-        handleMessage("remove");
-      } catch (error) {
-        console.log(error);
-      } finally {
-        setDeletingID(null);
-      }
-    },
-    [deleteTodo, handleMessage]
-  );
 
   const handleEditTodo = useCallback((todoItem) => {
     setSelectedTodo(todoItem);
@@ -57,36 +33,10 @@ const TodoListItem = ({ handleMessage }) => {
           </div>
         ) : (
           todoItems?.map((todoItem) => (
-            // <Card key={todoItem.id} className="col-lg-9  mx-auto mt-4">
-            //   <Card.Body>
-            //     <Card.Text>To do: {todoItem.todo}</Card.Text>
-            //     <Button
-            //       variant="warning"
-            //       onClick={(e) => handleEditTodo(todoItem)}
-            //       aria-label="Edit Todo"
-            //     >
-            //       <FontAwesomeIcon icon={solid("pen-to-square")} />
-            //     </Button>
-            //     <Button
-            //       variant="outline-danger"
-            //       className="d-inline-block position-absolute"
-            //       // style={{ top: "5px", right: "5px" }}
-            //       onClick={(e) => handleDeleteTodo(e, todoItem.id)}
-            //       disabled={deletingId === todoItem.id}
-            //     >
-            //       {deletingId === todoItem.id ? (
-            //         <Spinner animation="border" size="sm" />
-            //       ) : (
-            //         <FontAwesomeIcon icon={solid("trash")} />
-            //       )}
-            //     </Button>
-            //   </Card.Body>
-            // </Card>
             <TodoCard
               key={todoItem.id}
               todoItem={todoItem}
               handleEditTodo={handleEditTodo}
-              handleDeleteTodo={handleDeleteTodo}
               handleMessage={handleMessage}
             />
           ))
